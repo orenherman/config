61dcc1b9d89d196b97522b5ab121512ad257152c
Oren Herman <oren.herman@torq.io>
2024-06-03 15:00:21 +0300

    Fugitive 2

--- nvim/plugins/git.lua
+++ nvim/plugins/git.lua
@@ -1,5 +1,21 @@
 return {
   {
+    'SuperBo/fugit2.nvim',
+    opts = {
+      -- width = 70,
+      libgit2_path = '/opt/homebrew/lib/libgit2.dylib',
+    },
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'nvim-lua/plenary.nvim',
+    },
+    cmd = { 'Fugit2', 'Fugit2Diff', 'Fugit2Graph' },
+    keys = {
+      { '<leader>F', mode = 'n', '<cmd>Fugit2<cr>' },
+    },
+  },
+  {
     'github/copilot.vim',
   },
   {
